79778a02eedf4c6bce6d1008131c2d668d2b7525
Naiara Cassimiro <ncassimiro@byupathway.edu>
2026-07-15 10:56:41 -0300

    Semana03

--- scripts/local.js
+++ scripts/local.js
@@ -1,52 +1,31 @@
-// ===============================
-// Rodapé
-// ===============================
+const temperature = 8;
+const windSpeed = 6;
 
-// Ano atual
-const year = document.getElementById("year");
-year.textContent = new Date().getFullYear();
+function calcularSensacaoTermica(temp, vento) {
 
-// Última modificação da página
-const lastModified = document.getElementById("lastModified");
-lastModified.textContent = document.lastModified;
-
-
-// ===============================
-// Dados do Clima
-// ===============================
-
-const temperatura = 8;      // °C
-const velocidadeVento = 10; // km/h
-
-
-// ===============================
-// Função para calcular a sensação térmica
-// Fórmula para Celsius
-// ===============================
-
-function calcularSensacaoTermica(temperatura, velocidadeVento) {
     return (
         13.12 +
-        (0.6215 * temperatura) -
-        (11.37 * Math.pow(velocidadeVento, 0.16)) +
-        (0.3965 * temperatura * Math.pow(velocidadeVento, 0.16))
-    );
-}
-
-
-// ===============================
-// Exibir resultado
-// ===============================
-
-const windChill = document.getElementById("windchill");
-
-if (temperatura <= 10 && velocidadeVento > 4.8) {
-    const sensacao = calcularSensacaoTermica(
-        temperatura,
-        velocidadeVento
+        (0.6215 * temp) -
+        (11.37 * Math.pow(vento, 0.16)) +
+        (0.3965 * temp * Math.pow(vento, 0.16))
     ).toFixed(1);
 
-    windChill.textContent = `${sensacao} °C`;
-} else {
-    windChill.textContent = "N/A";
 }
+
+if (temperature <= 10 && windSpeed > 4.8){
+
+document.getElementById("windchill").textContent =
+`${calcularSensacaoTermica(temperature, windSpeed)} °C`;
+
+}
+else{
+
+document.getElementById("windchill").textContent = "N/A";
+
+}
+
+document.getElementById("currentyear").textContent =
+new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+document.lastModified;
