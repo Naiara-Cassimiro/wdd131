d7bc689074eab8867008be158eb30bbe9a16a3d0
Naiara Cassimiro <ncassimiro@byupathway.edu>
2026-07-16 16:57:56 -0300

    Semana03

--- scripts/local.js
+++ scripts/local.js
@@ -2,30 +2,23 @@ const temperature = 8;
 const windSpeed = 6;
 
 function calcularSensacaoTermica(temp, vento) {
-
     return (
         13.12 +
         (0.6215 * temp) -
         (11.37 * Math.pow(vento, 0.16)) +
         (0.3965 * temp * Math.pow(vento, 0.16))
     ).toFixed(1);
-
 }
 
-if (temperature <= 10 && windSpeed > 4.8){
-
-document.getElementById("windchill").textContent =
-`${calcularSensacaoTermica(temperature, windSpeed)} °C`;
-
-}
-else{
-
-document.getElementById("windchill").textContent = "N/A";
-
+if (temperature <= 10 && windSpeed > 4.8) {
+    document.getElementById("windchill").textContent =
+        `${calcularSensacaoTermica(temperature, windSpeed)} °C`;
+} else {
+    document.getElementById("windchill").textContent = "N/A";
 }
 
-document.getElementById("currentyear").textContent =
-new Date().getFullYear();
+// Ano atual
+document.getElementById("year").textContent = new Date().getFullYear();
 
-document.getElementById("lastModified").textContent =
-document.lastModified;
+// Última modificação
+document.getElementById("lastModified").textContent = document.lastModified;
